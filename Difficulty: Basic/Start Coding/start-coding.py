7ac38cd1247312f51c0a60546cbfaded59782e33
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-03-07 23:58:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Start Coding/start-coding.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def printHello(self) -> None:
+        # code here
+        print("Hello World")
+
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        obj = Solution()
+        obj.printHello()
+        print("~")
+
+# } Driver Code Ends
