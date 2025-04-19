8a03225b474b655f89ca693aa18fedfcf8615f9a
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-04-20 00:51:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Only Repetitive Element from 1 to n-1/find-only-repetitive-element-from-1-to-n1.py
@@ -0,0 +1,20 @@
+#User function Template for python3
+class Solution:
+    def findDuplicate(self, arr):
+        #code here
+        return (sum(arr)-sum(set(arr)))
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+
+        ob = Solution()
+        print(ob.findDuplicate(arr))
+        print("~")
+
+# } Driver Code Ends
