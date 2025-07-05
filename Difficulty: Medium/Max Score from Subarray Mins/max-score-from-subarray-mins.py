0bac4798102f9e99166b9d304de46c16d8650a76
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-07-05 10:54:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSum(self, arr):
+        ans=float('-inf')
+        for i in range(len(arr)-1):
+            if arr[i]+arr[i+1]> ans:
+                ans=arr[i]+arr[i+1]
+        return ans
+    
