013ff52d0fe732ff1f94e15400c4fe77576670e4
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-07-18 22:55:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/LCM Triplet/lcm-triplet.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lcmTriplets(self, n):
+        if n <= 2:
+            return n
+        if n & 1: 
+            return n * (n - 1) * (n - 2)
+        else:
+            if n % 3:
+                return n * (n - 1) * (n - 3)
+            else:
+                return (n - 1) * (n - 2) * (n - 3)
