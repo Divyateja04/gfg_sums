150a550b14f2d0ab98643e7051cb9e10d590e0ab
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2025-08-14 09:54:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Count Reverse Pairs/count-reverse-pairs.py
@@ -0,0 +1,11 @@
+import bisect
+class Solution:
+    def countRevPairs(self, arr):
+        cnt=0
+        sorted_arr=[]
+        for e in arr:
+            searched = e*2
+            i=bisect.bisect_right(sorted_arr,searched)
+            cnt+=len(sorted_arr)-i
+            bisect.insort_left(sorted_arr,e)
+        return cnt
