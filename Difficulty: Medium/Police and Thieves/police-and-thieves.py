dc38caabf9c4d54b505e488f53e055c46f467842
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-01-14 23:07:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Police and Thieves/police-and-thieves.py
+++ Difficulty: Medium/Police and Thieves/police-and-thieves.py
@@ -1,23 +1,26 @@
 class Solution:
     def catchThieves(self, arr, k):
-        n = len(arr)
-        i = 0
-        j = 0
-        res = 0
-        
-        while i < n and j < n:
-            while i < n and arr[i] != 'P':
-                i += 1
-            while j < n and arr[j] != 'T':
-                j += 1
-            if i < n and j < n and abs(i - j) <= k:
-                res += 1
-                i += 1
-                j += 1
-            elif j < n and j < i:
-                j += 1
-            elif i < n and i < j:
-                i += 1
-        
-        return res
-
+        try:
+            t=arr.index('T')
+            p=arr.index('P')
+        except:
+            return 0
+        ret=0
+        while 1:
+            if abs(p-t)<=k:
+                ret+=1
+                try:
+                    t=arr.index('T',t+1)
+                    p=arr.index('P',p+1)
+                except:
+                    return ret
+            elif t<p:
+                try:
+                    t=arr.index('T',t+1)
+                except:
+                    return ret
+            else:
+                try:
+                    p=arr.index('P',p+1)
+                except:
+                    return ret
