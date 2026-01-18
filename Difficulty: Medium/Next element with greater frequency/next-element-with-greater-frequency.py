b4a8deb6dacffd46668300cbe130de8e6e908943
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-01-18 22:45:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.py
+++ Difficulty: Medium/Next element with greater frequency/next-element-with-greater-frequency.py
@@ -1,19 +1,12 @@
-from collections import defaultdict
 class Solution:
-    def findGreater(self, arr):
-        mp=defaultdict(int)
-        for i in arr:
-            mp[i]+=1
-        result =[]
-        stack=[]
-        for i in range(len(arr)-1,-1,-1):
-            curr=arr[i]
-            while stack and stack[-1][-1]<=mp[curr]:
-                stack.pop()
-            if not stack:
-                result.append(-1)
-            else:
-                result.append(stack[-1][0])
-            stack.append((curr,mp[curr]))
-        return result[::-1]
+    def nextFreqGreater(self, arr):
+        from collections import Counter
+        cnt=Counter(arr)
+        ret=[-1]*len(arr)
+        stk=[]
+        for ix,ve in enumerate(arr):
+            while stk and cnt[arr[stk[-1]]]<cnt[ve]:
+                ret[stk.pop()]=ve
+            stk.append(ix)
+        return ret
         
