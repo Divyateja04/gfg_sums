0b14f21a0d71e6349c9b0a89b39dba855d87f9a4
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-01-28 22:56:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Subset With Target Sum II/count-subset-with-target-sum-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countSubset(self, arr, k):
+        from collections import Counter
+        n = len(arr)
+        mid = n // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        def subset_sums(nums):
+            sums = [0]
+            for x in nums:
+                sums += [s + x for s in sums]
+            return sums
+
+        left_sums = subset_sums(left)
+        right_sums = subset_sums(right)
+
+        right_count = Counter(right_sums)
+
+        ans = 0
+        for s in left_sums:
+            ans += right_count[k - s]
+
+        return ans
