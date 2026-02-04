bfee23c7ca9196f136b8992716a9c17da4155840
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-02-04 22:21:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.py
+++ Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.py
@@ -1,3 +1,3 @@
 class Solution:
     def getLastMoment(self, n, left, right):
-        return max(max(left) if left else 0, max(map(lambda x: n - x, right)) if right else 0)
+        return max(max(left) if left else 0,n-min(right) if right else 0)
