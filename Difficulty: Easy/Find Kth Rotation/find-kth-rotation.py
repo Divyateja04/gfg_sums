fda5ac8623fdfa0e570d43387740dd47440ee5cc
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-02-09 22:30:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findKRotation(self, arr):
+        low, high = 0, len(arr) - 1
+        while low < high:
+            mid = (low + high) // 2
+            if arr[mid] > arr[high]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
