de249c60aeca5e6ee7724ee4d6d5ac239e2e25fd
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-02-25 21:38:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.py
@@ -1,18 +1,16 @@
 class Solution:
     def longestSubarray(self, arr, k):
         n = len(arr)
-        trans = [1 if x > k else -1 for x in arr]
-        prefix = 0
-        first_occurrence = {0: -1}
-        max_len = 0
+        preSum={0:-1}
+        curr = 0
+        ans = 0
         for i in range(n):
-            prefix += trans[i]
-            if prefix > 0:
-                max_len = i + 1
-            if (prefix - 1) in first_occurrence:
-                prev_index = first_occurrence[prefix - 1]
-                max_len = max(max_len, i - prev_index)
-            if prefix not in first_occurrence:
-                first_occurrence[prefix] = i
-        
-        return max_len
+            curr += (1 if arr[i]>k else -1)
+            if curr<= 0:
+                if curr-1 in preSum:
+                    ans=max(ans,i-preSum[curr-1])
+            else:
+                ans = max(ans,i+1)
+            if curr not in preSum :
+                preSum[curr] = i
+        return ans
