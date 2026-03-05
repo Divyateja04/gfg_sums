4b4781517eb7fd718a684044f87b21e66b1ac026
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-03-05 20:40:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.py
+++ Difficulty: Medium/Longest Substring with K Uniques/longest-substring-with-k-uniques.py
@@ -1,21 +1,20 @@
+from collections import defaultdict
 class Solution:
     def longestKSubstr(self, s, k):
-        from collections import defaultdict
-        mp=defaultdict(int)
-        n=len(s)
-        left=0
-        count=0
-        for r in range(n):
-            mp[s[r]] +=1
-            while len(mp)>k and left <n:
-                if mp[s[left]]==1:
-                    del mp[s[left]]
-                else:
-                    mp[s[left]] -=1
-                left+=1
-            if len(mp)==k:
-                count=max(count,r-left+1)
-                
-        return -1 if count==0 else count
-        
-        
+        dic=defaultdict(int)
+        i=0
+        j=0
+        maxi=-1
+        while j<len(s):
+            dic[s[j]]+=1
+            if len(dic)==k:
+                maxi=max(maxi,j-i+1)
+            elif len(dic)>k:
+                dic[s[i]]-=1
+                if dic[s[i]]==0:
+                    del dic[s[i]]
+                i+=1
+            j+=1
+        if len(dic)==k:
+            maxi=max(maxi,j-i)
+        return maxi
