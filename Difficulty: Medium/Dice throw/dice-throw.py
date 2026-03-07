cb2c8c7b981a258fbce7aba91d10e9bdb9fdfa38
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-03-07 23:18:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dice throw/dice-throw.py
+++ Difficulty: Medium/Dice throw/dice-throw.py
@@ -1,11 +1,13 @@
 class Solution:
     def noOfWays(self, m,n,x):
-        dp = [[0]*(n+1) for _ in range(x+1)]
-        dp[0][0] = 1
-        for s in range(1, x+1):
-            for i in range(1, n+1):
-                for k in range(1, m+1):
-                    if s-k >= 0:
-                        dp[s][i] += dp[s-k][i-1]
-        
-        return dp[-1][-1]
+        dp =[0] * (x + 1)
+        dp[0] = 1
+        for _ in range(n):
+            new_dp = [0] * ( x + 1)
+            for s in range(x + 1):
+                if dp[s] != 0:
+                    for face in range(1, m + 1):
+                        if s + face <= x:
+                            new_dp[s + face] += dp[s]
+            dp = new_dp
+        return dp[x]
