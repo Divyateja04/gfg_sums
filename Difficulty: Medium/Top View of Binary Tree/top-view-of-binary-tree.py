2831f2534439f9de3b35b4f7914395b5c5ad92f0
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-03-14 23:49:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topView(self, root):
+        from collections import deque
+        left, right = [], []
+        s = deque([(root, 0)])
+        while s:
+            node, i = s.popleft()
+            if node is None:
+                continue
+            if i < 0:
+                if -i > len(left):
+                    left.append(node.data)
+            else:
+                if i == len(right):
+                    right.append(node.data)
+            s.append((node.left, i - 1))
+            s.append((node.right, i + 1))
+        left.reverse()
+        left.extend(right)
+        return left
