f07e61c4d7cb889a9aab4a780bea88c0fc757767
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-03-22 23:12:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotten Oranges/rotten-oranges.py
+++ Difficulty: Medium/Rotten Oranges/rotten-oranges.py
@@ -1,36 +1,25 @@
 class Solution:
-	def orangesRotting(self, mat):
-		#Code here
-		q = [(i,j) for i, r in enumerate(mat) for j,e in enumerate(r) if e ==2]
-        m,n,t = len(mat), len(mat[0]), 0
-        while q:
-            nq = []
-            for r0, c0, in q:
-                for r,c in ((r0+1, c0), (r0-1, c0), (r0, c0+1), (r0, c0-1)):
-                    if 0 <= r < m and 0 <= c < n and mat[r][c] == 1:
-                        nq.append((r,c))
-                        mat[r][c] = 2
-            if nq:
-                q = nq
-                t += 1
-            q = nq
-        return -1 if any([e == 1 for r in mat for e in r]) else t
+	def orangesRot(self, mat):
+		r, c = len(mat), len(mat[0])
+        q, fresh = [], 0
 
-#{ 
- # Driver Code Starts
-from queue import Queue
-
-T = int(input())
-for i in range(T):
-    n = int(input())
-    m = int(input())
-    mat = []
-    for _ in range(n):
-        a = list(map(int, input().split()))
-        mat.append(a)
-    obj = Solution()
-    ans = obj.orangesRotting(mat)
-    print(ans)
-    print("~")
-
-# } Driver Code Ends
+        for i in range(r):
+            for j in range(c):
+                if mat[i][j] == 2: 
+                    q.append((i, j))
+                elif mat[i][j] == 1:
+                    fresh += 1
+        time = 0
+        dirs = [(1,0),(-1,0),(0,1),(0,-1)]
+        while q and fresh:
+            new_q = []
+            for x, y in q:
+                for dx, dy in dirs:
+                    nx, ny = x+dx, y+dy
+                    if 0 <= nx < r and 0 <= ny < c and mat[nx][ny] == 1:
+                        mat[nx][ny] = 2
+                        fresh -= 1
+                        new_q.append((nx, ny))
+            q = new_q
+            time += 1
+        return time if fresh == 0 else -1
