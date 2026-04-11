dfe4b4d76f5234364a36e3ae3602f1a2cc7c86c3
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-04-11 23:02:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count increasing Subarrays/count-increasing-subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countIncreasing(self, arr):
+        i=1
+        j=0
+        ans=0
+        while(i<len(arr)):
+            if arr[i]>arr[i-1]:
+                if i>j:
+                    ans+=(i-j)
+            else:
+                j=i
+            i+=1
+        return ans
