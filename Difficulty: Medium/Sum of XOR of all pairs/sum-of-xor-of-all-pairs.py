5736c24c9f986df4309bce60fc7666c81bbaacd9
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-05-05 23:28:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of XOR of all pairs/sum-of-xor-of-all-pairs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sumXOR(self, arr):
+        n, total = len(arr), 0
+        for i in range(32):
+            count1 = sum((num >> i) & 1 for num in arr)
+            total += count1 * (n - count1) * (1 << i)
+        return total
