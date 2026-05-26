ddd1060dcc496b9c3fddfea91cd7e82c8e5ab54f
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-05-26 22:22:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Minimum Toggle to Partition/minimum-toggle-to-partition.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minToggle(self, arr):
+        zeros = len(arr)-sum(arr)
+        min_toggles = zeros
+        curr_toggles = zeros
+        for num in arr:
+            if num == 1:
+                curr_toggles += 1
+            else:
+                curr_toggles -= 1
+            min_toggles = min(min_toggles, curr_toggles)
+        return min_toggles
