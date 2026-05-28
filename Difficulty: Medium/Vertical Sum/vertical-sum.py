66d2ce7c7067d101170ab9a5d6b506753c7fe3e8
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-05-28 22:19:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Vertical Sum/vertical-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def verticalSum(self, root):
+        mp = {}
+        self.mn = 0
+        self.mx = 0
+        def dfs(node, hd):
+            if not node:
+                return
+            mp[hd] = mp.get(hd, 0) + node.data
+            self.mn = min(self.mn, hd)
+            self.mx = max(self.mx, hd)
+            dfs(node.left, hd - 1)
+            dfs(node.right, hd + 1)
+        dfs(root, 0)
+        ans = []
+        for i in range(self.mn, self.mx + 1):
+            ans.append(mp[i])
+        return ans
