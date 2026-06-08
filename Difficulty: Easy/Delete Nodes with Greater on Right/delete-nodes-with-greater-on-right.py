09846eb61c6826f254712fc1628a44ee31f25e27
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-06-08 22:11:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Nodes with Greater on Right/delete-nodes-with-greater-on-right.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rev(self,head):
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+    def compute(self,head):
+        if head.next==None:
+            return head
+        head1 = self.rev(head)
+        prev = head1
+        curr = head1.next
+        while curr:
+            if curr.data<prev.data:
+                prev.next = curr.next
+            else:
+                prev=curr
+            curr = curr.next
+        head = self.rev(head1)
+        return head
