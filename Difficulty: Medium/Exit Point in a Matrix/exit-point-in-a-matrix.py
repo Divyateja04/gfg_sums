238d4b0ed283c62aed8080af5bc07e40ff8b7c38
DivyaTeja.Komiri <komiridivyateja321@gmail.com>
2026-06-14 23:13:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exit Point in a Matrix/exit-point-in-a-matrix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def exitPoint(self, mat):
+        dir = ((0, 1), (1, 0), (0, -1), (-1, 0))
+        dir_ind = 0
+        r, c = 0, 0
+        last = None
+        while 0 <= r < len(mat) and 0 <= c < len(mat[0]):
+            last = (r, c)
+            if mat[r][c] == 1:
+                dir_ind += 1
+                mat[r][c] = 0
+                
+            dx, dy = dir[dir_ind % len(dir)]
+            r = r + dx
+            c = c + dy
+        return last
